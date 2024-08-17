13e68d76745efbb38e32571fa85c46fdc9e93f27
Nor-r <nooradhiiin@gmail.com>
2024-08-18 03:24:06 +0500

    standard page top slider update

--- src/components/global/Slider.tsx
+++ src/components/global/Slider.tsx
@@ -1,38 +1,29 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import Slider from "react-slick";
 interface CarouselProps {
   items: string[];
 }
 
-const Carousel: React.FC<CarouselProps> = ({items}) => {
-  var settings = {
-    dots: true,
+const Carousel: React.FC<CarouselProps> = ({ items }) => {
+  const settings = {
+    className: "center",
+    centerMode: true,
     infinite: true,
-    speed: 500,
+    centerPadding: "60px",
     slidesToShow: 1,
-    slidesToScroll: 1,
+    speed: 500,
+    autoplay: true,
+    autoplaySpeed: 5000
+
   };
   return (
     <div className="">
       <Slider {...settings}>
-        <div className="h-[70vh] bg-red-400">
-          <h3>1</h3>
-        </div>
-        <div className="h-[70vh] bg-red-400">
-          <h3>2</h3>
-        </div>
-        <div className="h-[70vh] bg-red-400">
-          <h3>3</h3>
-        </div>
-        <div className="h-[70vh] bg-red-400">
-          <h3>4</h3>
-        </div>
-        <div className="h-[70vh] bg-red-400">
-          <h3>5</h3>
-        </div>
-        <div className="h-[70vh] bg-red-400">
-          <h3>6</h3>
-        </div>
+        {items.map((img: any) =>
+          <div className="h-[80vh] px-3 focus:outline-none">
+            <img className="w-full h-full object-cover rounded-md" src={img}></img>
+          </div>)}
+
       </Slider>
     </div>
   );
